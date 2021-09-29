e5dfba0fa08ca4c80c5919c4629b5a7a89c0e812
anggar95 <kanan.anggar@gmail.com>
2021-09-29 19:53:52 +0600

    Change Login and Register Forms

--- frontend/views/site/requestPasswordResetToken.php
+++ frontend/views/site/requestPasswordResetToken.php
@@ -9,23 +9,40 @@ use yii\captcha\Captcha;
 
 $this->title = Yii::t('users', 'REQUEST_PASSWORD_RESET');
 $this->params['breadcrumbs'][] = $this->title;
+$this->registerCssFile('/css/signup.css');
 ?>
 <style>
     body {
-        background: #E5E5E5 !important;
+        background: url(/img/register/reg-img.svg)no-repeat center;
+        background-size: cover;
     }
+    .form-control{
+        width: 100%;
+        height: 40px;
+        border-radius: 4px;
+
+        padding-left: 16px;
+
+        background: #292344;
+        color: #fff;
+        border: 1px solid #292344;
+    }
+
 </style>
-<div class="box__reg">
-    <div class="fon__reg">
-        <div class="reg__header">
-            <h3 class="w7">Восстановление пароля</h3>
+<div class="register-center-block">
+    <div class="center">
+        <img src="/img/logo.svg" alt="">
+    </div>
+    <div class="register-content fon-main txt-white">
+        <div class="register-name center">
+            <h4>Восстановление пароля</h4>
         </div>
         <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
             <?= $form->field($model, 'email',['options'=> ['class'=>'field mt-5']])->textInput(['placeholder'=>'E-mail', 'class'=>'form-control'])->label(false) ?>
             <div class="">
                 <div class="checkbox__group mt-4">
                     <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                        'template' => '<div class="row"><div class="col-4">{image}</div><div class="col-6">{input}</div></div>',
+                        'template' => '<div class="row"><div class="col-4">{image}</div><div class="col-6 ml-1">{input}</div></div>',
                     ])->label(false) ?>
                 </div>
 
@@ -35,11 +52,11 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
 
                 <div class=" mt-1">
-                    <input class="fon-grey btn__small" type="submit" value="<?=Yii::t('users', 'SEND')?>" style="width: 100%;">
+                    <input class="fon-btn-green btn__small" type="submit" value="<?=Yii::t('users', 'SEND')?>" style="width: 100%;">
                 </div>
 
-                <div class=" text-center mt-5 text__small">
-                    <p class="">У вас есть аккаунт? <a href="/site/login">Войти</a></p>
+                <div class="center-line mt-4">
+                    <p class="txt-A3D1">У вас есть аккаунт? <a class="txt-green w4" href="/site/login">Войти</a></p>
                 </div>
         <?php ActiveForm::end(); ?>
     </div>
--- frontend/views/site/retryConfirmEmail.php
+++ frontend/views/site/retryConfirmEmail.php
@@ -7,8 +7,14 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = Yii::t('users', 'EMAIL_CONFIRMATION');
 $this->params['breadcrumbs'][] = $this->title;
+$this->registerCssFile('/css/signup.css');
 ?>
-
+<style>
+    body {
+        background: url(/img/register/reg-img.svg)no-repeat center;
+        background-size: cover;
+    }
+</style>
 <div id="login-full-wrapper">
     <div class="container">
         <div class="row">
@@ -18,17 +24,17 @@ $this->params['breadcrumbs'][] = $this->title;
                         <div class="row">
                             <div class="col-xs-12">
                                 <header id="login-header">
-                                    <div id="login-logo">
-                                        Best Road Invest
+                                    <div class="center">
+                                        <img src="/img/logo.svg" alt="">
                                     </div>
                                 </header>
                                 <div id="login-box-inner">
                                     <?php $form = ActiveForm::begin(['id' => 'retry-confirm-email-form']); ?>
-                                    <?= $form->field($model, 'email')->textInput(['placeholder'=>'E-mail'])->label(false) ?>
+                                    <?= $form->field($model, 'email')->textInput(['placeholder'=>'E-mail', 'class'=>'input'])->label(false) ?>
                                     <div class="form-group">
                                         <div class="g-recaptcha" data-sitekey="6Le_8x4UAAAAAI1-lV1rwMepHkh7PbrlrsEz5LUt"></div>
                                     </div>
-                                    <input class="btn btn-success" type="submit" value="<?=Yii::t('users', 'SEND')?>">
+                                    <input class="btn fon-btn-green" type="submit" value="<?=Yii::t('users', 'SEND')?>">
                                     <?php ActiveForm::end(); ?>
 
                                 </div>
